8c711f9a08cc899211afd13c466359971928b9b7
Jimena Miguez <jimeblau@gmail.com>
2022-09-07 13:06:29 +0200

    Finish initializing out store

--- components/Admin/AdminPostForm.vue
+++ components/Admin/AdminPostForm.vue
@@ -1,13 +1,17 @@
 <template>
-  <!-- 2) When we submit the form we call the 
-  onSave method, which is here in this component. -->
   <form @submit.prevent="onSave">
     <AppControlInput v-model="editedPost.author">Author Name</AppControlInput>
     <AppControlInput v-model="editedPost.title">Title</AppControlInput>
-    <AppControlInput v-model="editedPost.thumbnailLink">Thumbnail Link</AppControlInput>
+    <!-- 3) Update the name of the property to just thumbnail. -->
+    <AppControlInput v-model="editedPost.thumbnail">Thumbnail Link</AppControlInput>
     <AppControlInput
       control-type="textarea"
       v-model="editedPost.content">Content</AppControlInput>
+    <!-- 4) Add a textarea input which will allow us to
+    post and fetch a preview text-->
+    <AppControlInput
+      control-type="textarea"
+      v-model="editedPost.previewText">Preview Text</AppControlInput>
     <AppButton type="submit">Save</AppButton>
     <AppButton
       type="button"
@@ -39,9 +43,22 @@ export default {
         : {
             author: "",
             title: "",
-            thumbnailLink: "",
-            content: ""
+            /* 2) Change the name of the property thumbnailLink to 
+            thumbnail. So that we´re in line with other components
+            where we try to access this property. */
+            thumbnail: "",
+            content: "",
+            /* 5) We nee to add the prveiw text
+            filed we just added in the template
+            to the editedPost object, so that
+            we can send it to the database. */
+            previewText: ""
           }
+          /* 6) Go to firebase, delate the firt post
+          and create a new one clickin on the create 
+          post button in our app. Now if I manually 
+          navigate back to the main page, I will see
+          the new post I submitted.*/
     };
   },
   methods: {
